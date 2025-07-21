dd7bf3043336533f921110d7eca1c2b9bee605f5
Bunyodjon Raxmonov <raxmonovbunyodjon5081999@gmail.com>
2025-07-22 00:18:39 +0500

    feat: Added reverse array solution for natural numbers

---
+++ 01-kotlin-basics/08-lesson/src/Topshiriq-1.kt
@@ -0,0 +1,29 @@
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    println("Nechta natural son bo`lishi kerak: ")
+    var N = scanner.nextInt()
+
+    var sonN = IntArray(N)
+
+    println("$N ta natural sonlarni kiriting: ")
+    for (item in 0 until N) {
+        while (true) {
+            print("${item + 1}-son: ")
+            var son = scanner.nextInt()
+            if (son > 0) {
+                sonN[item] = son
+                break
+            } else {
+                println("X xatolik: Faqat natural son kiriting (1,2,3,4,,,,)")
+            }
+        }
+    }
+
+    println("Teskari tartibdagi sonlar:")
+    for (item in sonN.reversed()) {
+        println(item)
+    }
+}
+
