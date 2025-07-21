4250d79a04fc7d44578d585857f77cb824fddb72
Bunyodjon Raxmonov <raxmonovbunyodjon5081999@gmail.com>
2025-07-21 10:46:53 +0500

    I added new file lesson

--- 01-kotlin-basics/08-lesson/src/Main.kt
+++ 01-kotlin-basics/08-lesson/src/Main.kt
@@ -1,7 +1,12 @@
 fun main(args: Array<String>) {
     var a = arrayOf("FIFA", 22, 56.55, '#', true)
 
-    for (item in a){
-        println(item)
+    for (item in a.indices){
+        println(a[item])
+    }
+
+    println("@@@@@@@@@@@@@@@@@")
+    for (i in a){
+        println(i)
     }
 }
---
+++ 01-kotlin-basics/08-lesson/src/Main1.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+
+    var a = arrayOf<Int>(16,21,35,50)
+
+//    a[1] = 88
+//    println(a[1])
+
+    var b = arrayOf<String>("Kotlin","Python","Java","Android")
+
+//    println(b.contains("Python"))
+//    println(b.size)
+
+    println(b.first())
+    println(b.last())
+}
